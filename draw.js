f48d44807604ac7c27c812166ef14dc5593ea349
yxzh614 <526686022@qq.com>
2018-05-08 13:14:17 +0800

    简单的jump功能 方块碰撞

--- draw.js
+++ draw.js
@@ -1,12 +1,14 @@
 class Drawer {
   constructor (id) {
-    this.el = document.querySelector(id)
-    this.chractor = {
+    this.el = document.getElementById(id)
+    this.ctx = this.el.getContext('2d')
+    this.character = {
       x: 240,
       y: 240,
       height: 20,
       width: 20,
       jump (voice) {
+        console.log('jump')
         for(let i = 0; i < 40; i++) {
           setTimeout(() => {
             if (i < 20) {
@@ -18,22 +20,79 @@ class Drawer {
         }
       }
     }
-    this.blocks = [{
-      width: 20,
-      height: 20,
-      x: 500,
-      y: 240,
+    this.blocks = {
+      _blocks: [{
+        width: 20,
+        height: 20,
+        x: 500,
+        y: 240,
+      }],
       move (direction) {
         switch (direction) {
           case 'left': {
-            if (this.x < 0) {
-              this.x = 500
-            }
-            this.x -= 5
+            this._blocks.map((item, index) => {
+              if (item.x < 0) {
+                item.x = 500
+              }
+              item.x -= 5
+            })
           } break
         }
       }
-    }]
+    }
     this.speed = 1
+    this.running = false
+  }
+  draw (x) {
+    this.ctx.clearRect(0, 0, 500, 500)
+    this.blocks.move('left')
+    this.ctx.fillStyle = 'green'
+    this.blocks._blocks.map((item) => {
+      this.ctx.fillRect(item.x, item.y, item.width, item.height)
+    })
+    this.ctx.fillStyle = 'red'
+    this.ctx.fillRect(this.character.x, this.character.y, this.character.width, this.character.height)
+    if (this.isCrash()) {
+      this.onFinish()
+    }
+    if (this.running) {
+      window.requestAnimationFrame(this.draw.bind(this))
+    }
+  }
+  start () {
+    this.running = true
+    window.requestAnimationFrame(this.draw.bind(this))
+  }
+  stop () {
+    this.running = false
+  }
+  isCrash () {
+    let r = false
+    this.blocks._blocks.map((item) => {
+      if (crash(item, this.character)) {
+        r = true
+      }
+    })
+    return r
+  }
+  onFinish () {
+  }
+
+}
+function crash (r1, r2) {
+  let r = {
+    x: r1.x,
+    y: r1.y,
+    w: r1.width + r2.width,
+    h: r1.height + r2.height,
+  }
+  let p = {
+    x: r2.x + r2.width,
+    y: r2.y + r2.height
+  }
+  if (p.x > r.x && p.y > r.y && p.x < r.x + r.w && p.y < r.y + r.y + r.h) {
+    return true
+  } else {
+    return false
   }
 }
